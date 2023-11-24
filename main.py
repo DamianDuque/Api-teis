f062cecc1ecbe202c0dd837f62fc39fbc6f00b1e
Damian <damianduquel@gmail.com>
2023-11-24 01:13:36 -0500

    deployment test

--- main.py
+++ main.py
@@ -3,28 +3,26 @@ from flask import Flask
 import psycopg2
 import os
 
-
-TESTQUERY = """SELECT * FROM "public"."CensusCollector";"""
-
+TESTQUERY = """SELECT "ECN", "CensusData", state
+	FROM public."CensusForm";"""
 
 load_dotenv()
-
 
 app = Flask(__name__)
 url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(url)
-
 
 @app.get("/")
 def home():
     return "Hello, world!"
 
 @app.get("/api/room/")
-def get_room_all(room_id):
+def get_room_all():
     with connection:
         with connection.cursor() as cursor:
-            ECNnum = cursor.execute(TESTQUERY)
-    return {"name": ECNnum}
+            cursor.execute(TESTQUERY)
+            resultquery = cursor.fetchall()
+    return {"result": resultquery}
 
 if __name__ == "__main__":
     app.run(debug=True)
